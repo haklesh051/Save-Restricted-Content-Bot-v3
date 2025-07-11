b903aa01ce75ab0711ca551290e02e2007b6cfaf
haklesh051 <hakleshbairwa051@gmail.com>
2025-07-11 16:26:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ YTUB_COOKIES = """
 
 API_ID = os.getenv("API_ID", "23977225")
 API_HASH = os.getenv("API_HASH", "969c399e6aea5032314a2e17cab428be")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7633147515:AAGehPj6nS4bw72beMusMbOuyDRLFi5cdjY")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7979187726:AAHPd5VRTTrAkKFt3GZXHqQuoplYe9xBZsg")
 MONGO_DB = os.getenv("MONGO_DB", "")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "7681589139").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
