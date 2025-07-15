c3ff9851d5bf427c2549602f25517b2cdaa868b8
haklesh051 <hakleshbairwa051@gmail.com>
2025-07-15 15:25:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,4 +34,5 @@ FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/bot_haklesh") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/Mr_x_tg")
+user_id = list(map(int, os.getenv("user_id", "7681589139").split()))
 
